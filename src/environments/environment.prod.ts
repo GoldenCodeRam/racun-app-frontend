0a2a2dba1e51d9bec04243a171f3b81295bb98ca
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2022-12-10 00:24:57 -0500

    Update: Production file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+    production: true,
+    apiUrl: "https://racun-backend-production.up.railway.app/api",
+    backendUrl: "https://racun-backend-production.up.railway.app",
 };
